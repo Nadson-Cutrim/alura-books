a18438c8b1983f736d3e0b501b5573466e7568e0
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-11 21:47:06 -0300

    Botão filtrar Livros Front-End

---
+++ app/metodoFilter.js
@@ -0,0 +1,8 @@
+const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront')
+
+btnFiltrarLivrosFront.addEventListener('click', filtrarLivrosFront)
+
+function filtrarLivrosFront() {
+    let livrosFiltrados  = livros.filter(livro => livro.categoria === 'front-end')
+    console.table(livrosFiltrados)
+}
