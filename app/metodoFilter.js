2945bdf8b2968c32796bc78bd54744be7009fc7a
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-12 14:33:30 -0300

    Filtrtando livros por categoria

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,24 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id) // Pega o elemento do botão clicado
     const categoria = elementoBtn.value
-    let livrosFiltrados  = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados  = categoria == 'disponivel' ?  filtrarPorDisponibilidade(): filtrarPorCategoria(categoria)
+    
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel') {
+       exibirValorTotalDeLivrosDisponiveiNaTela()
+    }   
+}
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDeLivrosDisponiveiNaTela(){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML =  `
+     <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>`
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,9 @@
 const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
 //Aqui estamos pegando o elemento HTML onde vamos inserir os livros
 function exibirOsLivrosNaTela(listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = '' // Limpando o HTML antes de inserir os livros
     elementoParaInserirLivros.innerHTML = '' // Limpando o HTML antes de inserir os livros
   listaDeLivros.forEach(livro => {
     let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel' // If ternario para  Verificar se o livro está disponível ou não
